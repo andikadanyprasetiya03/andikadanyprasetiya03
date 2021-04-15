381ca157a4301d1265fba63fcf4184ca433d16fc
andikadanyprasetiya03 <82603377+andikadanyprasetiya03@users.noreply.github.com>
2021-04-15 23:49:34 +0700

    Add files via upload

---
+++ Tugas 6 BIG DATA.py
@@ -0,0 +1,11 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Nama : Andika Dany Prasetiya \n")
+Nama : Andika Dany Prasetiya 
+
+>>> print ("Kelas : Teknik Informatika B \n")
+Kelas : Teknik Informatika B 
+
+>>> print ("Kampus : Universitas Narotama")
+Kampus : Universitas Narotama
+>>> 
